eb16f43c9242fb07ddd3b0f0a3c7e40e41e4b469
Gavin <tianyu_0723@hotmail.com>
2017-11-25 01:41:41 +0800

    添加了 axios 请求库，vee-validate 表单验证库，跨域请求库

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const koaBodyparser = require('koa-bodyparser')
 const koaJson = require('koa-json')
 const koaLogger = require('koa-logger')
 const controller = require('./server/controller')
+const cors = require('koa-cors');
 
 const app = new Koa()
-
 app.use(koaBodyparser())
 app.use(koaJson())
 app.use(koaLogger())
+app.use(cors())
 
 app.use(async function (ctx, next) {
   let start = new Date()
@@ -27,9 +28,3 @@ app.use(controller())
 module.exports = app.listen(3001, () => {
   console.log(`Koa is listening in 3001`)
 })
-
-// app.listen(3001, ()=>{
-//   "use strict"
-//   console.log('koa is listening in 3001')
-// })
-// module.exports = app;
--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from './router'
+import axios from 'axios'
+import VeeValidate, { Validator } from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue'
 
+Vue.prototype.$http = axios
+Validator.localize('zhCN', zhCN)
+Vue.use(VeeValidate, {locale: 'zhCN'})
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
