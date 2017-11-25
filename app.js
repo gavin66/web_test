42bcc4cf6830b2427416f9407596aedb432f11a1
Gavin <tianyu_0723@hotmail.com>
2017-11-25 09:07:16 +0800

    打包，生产环境使用

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ const koaJson = require('koa-json')
 const koaLogger = require('koa-logger')
 const controller = require('./server/controller')
 const cors = require('koa-cors');
+const path = require('path')
+const serve = require('koa-static')
+
 
 const app = new Koa()
+// 将 webpack 打包好的目录作为 koa 静态服务
+app.use(serve(path.resolve('dist')))
 app.use(koaBodyparser())
 app.use(koaJson())
 app.use(koaLogger())
+// 允许跨域请求
 app.use(cors())
+// 路由，控制器
+app.use(controller())
+
 
 app.use(async function (ctx, next) {
   let start = new Date()
@@ -23,7 +32,6 @@ app.on('error', function(err, ctx){
   console.log('server error', err)
 });
 
-app.use(controller())
 
 module.exports = app.listen(3001, () => {
   console.log(`Koa is listening in 3001`)
